309654eb0897afda5c0c7dbf9d6b0446da7ef82b
=Salman nazeer <salmanselu2002@gmail.com>
2021-12-05 07:01:34 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# node-js backend for simple messaging app
+
+Just testing some nodejs
+
+Backend hosted at: https://pethu-chat.herokuapp.com
+<br>Front end: https://pethu-chat.netlify.app
---
+++ main.js
@@ -0,0 +1,21 @@
+var http = require('http');
+
+var url = require('url');
+
+helpNeeded = false;
+var main = function(req, res)
+{   
+   
+   var q = url.parse(req.url, true).query;
+   if(q.pathname === '/'){  
+      res.write(helpNeeded);
+      res.end();
+   }
+   if(q.pathname === '/help'){
+      helpNeeded = true;
+   }
+   if(q.pathname === '/clear'){
+      helpNeeded = false;
+   }
+}
+http.createServer(main).listen(process.env.PORT || 4200);
