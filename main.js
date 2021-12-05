ee7d245dae3184e128c5ac1f157087e2ce213e0c
=Salman nazeer <salmanselu2002@gmail.com>
2021-12-05 07:44:59 +0530

    reordered to attempt to fix bug

--- main.js
+++ main.js
@@ -7,13 +7,14 @@ var main = function(req, res)
 {   
    
    var q = url.parse(req.url, true).query;
-   res.write(helpNeeded);
-   res.end();
+
    if(q.pathname === '/help'){
       helpNeeded = "true";
    }
    if(q.pathname === '/clear'){
       helpNeeded = "false";
    }
+   res.write(helpNeeded);
+   res.end();
 }
 http.createServer(main).listen(process.env.PORT || 4200);
